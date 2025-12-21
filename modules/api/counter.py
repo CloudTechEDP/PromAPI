086a3ea06d01d094d546127451938130373608a1
Celio De Jesus <celio.jesus01@outlook.com>
2025-12-21 02:31:43 -0300

    ajuste da label timestamp

--- modules/api/counter.py
+++ modules/api/counter.py
@@ -7,7 +7,10 @@ async def receive_counter_metric(job_name: str, request: Request):
         raw_body = (await request.body()).decode()
         counter_name = raw_body.strip().split('{')[0]
         labels_part = re.search(r'\{(.*)\}', raw_body)
+        unique_label = datetime.now().strftime("%Y%m%dT%H%M%S_%f")
         labels = {}
+        labels["timestamp"] = unique_label
+
         if labels_part:
             labels_str = labels_part.group(1)
             label_pairs = labels_str.split(',')
--- modules/routes/index.py
+++ modules/routes/index.py
@@ -60,9 +60,9 @@ async def get_all_metrics():
             response.append(f"# TYPE {c.metric} counter")
             emitted_types.add(c.metric)
         
-        unique_label = datetime.now().strftime("%Y%m%dT%H%M%S_%f")
+        # unique_label = datetime.now().strftime("%Y%m%dT%H%M%S_%f")
         if labels:
-            labels["ts"] = unique_label
+            # labels["ts"] = unique_label
             labels_str = ', '.join([f'{k}="{v}"' for k, v in labels.items()])
             response.append(f'{c.metric}{{{labels_str}}} {valc}')
         else:
